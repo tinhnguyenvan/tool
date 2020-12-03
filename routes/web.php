f43e518905822b8c5e123e1b215e383cbfe0ddb4
Tình Nguyễn Văn <tinhnguyenvan91@gmail.com>
2020-12-03 14:36:50 +0700

    optimize menu sidebar

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,5 @@ use Illuminate\Support\Facades\Route;
 use TinhPHP\Tool\Http\Controllers\ToolController;
 
 Route::get('sl/{short_link}', [ToolController::class, 'shortLink']);
-Route::get(
-    'tool',
-    function () {
-        return redirect()->route('icon-facebook');
-    }
-);
 Route::get('tool/icon-facebook', [ToolController::class, 'emoji'])->name('icon-facebook');
 Route::get('tool/generate-qrcode', [ToolController::class, 'generateQrcode'])->name('generate-qrcode');
