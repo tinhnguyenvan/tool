aa4c6c919bfcad25d5aa23091dfa32b213037695
Tình Nguyễn Văn <tinhnguyenvan91@gmail.com>
2020-12-07 14:13:36 +0700

    update connect plugin on/off

--- config/constant.php
+++ config/constant.php
@@ -3,15 +3,19 @@
 return [
     'MENU_APP' => [
         'nav.menu_left.tool' => [
+            'plugin' => 'tool',
             'title' => 'nav.menu_left.tool',
             'url' => '',
             'icon' => 'fa fa-code',
             'child' => [
                 [
+                    'plugin' => 'tool',
                     'title' => 'nav.menu_left.tool_qr_code',
                     'url' => 'tools/qr_code',
                     'icon' => 'fa fa-qrcode',
-                ],        [
+                ],
+                [
+                    'plugin' => 'tool',
                     'title' => 'nav.menu_left.tool_short_link',
                     'url' => 'tools/short_link',
                     'icon' => 'fa fa-link',
