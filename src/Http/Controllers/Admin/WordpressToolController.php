ee94ac0c752318d6e6c855637cc53a4c1efdc955
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-05-16 00:57:30 +0700

    get all item

--- src/Http/Controllers/Admin/WordpressToolController.php
+++ src/Http/Controllers/Admin/WordpressToolController.php
@@ -5,6 +5,7 @@ namespace TinhPHP\Tool\Http\Controllers\Admin;
 
 use App\Models\Language;
 use App\Models\Post;
+use App\Models\PostTranslation;
 use App\Services\MediaService;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
@@ -37,23 +38,35 @@ class WordpressToolController extends AdminToolController
         $params = $request->all();
 
         $client = new Client();
-        $response = $client->get($params['link_api_post']);
-        $items = json_decode($response->getBody(), true);
-
-        if (!empty($items)) {
-            foreach ($items as $item) {
+        $page = 1;
+        $totalItem = 0;
+        do {
+            $response = $client->get($params['link_api_post'] . '?page=' . $page);
+            $result = json_decode($response->getBody(), true);
+            foreach ($result as $item) {
+                $totalItem++;
                 $formData['creator_id'] = Auth::id() ?? 0;
                 foreach (Language::loadLanguage() as $code => $textLanguage) {
+                    $myPost = PostTranslation::query()->where('locale', $code)->where('slug', $item['slug'])->first();
                     $formData[$code]['slug'] = $item['slug'];
                     $formData[$code]['title'] = $item['title']['rendered'];
+                    $formData[$code]['detail'] = $item['content']['rendered'];
+                    $formData[$code]['summary'] = $item['excerpt']['rendered'];
+
+                    if (!empty($myPost->slug)) {
+                        $myObject = Post::query()->find($myPost->post_id);
+                        $myObject->update($formData);
+                    } else {
+                        $myObject = new Post($formData);
+                        $myObject->save($formData);
+                    }
                 }
-                $myObject = new Post($formData);
-                $myObject->save($formData);
-                if (!empty($dataCondition)) {
-                    Post::query()->updateOrCreate($dataCondition, $formData);
-                }
-                var_dump($formData);
             }
-        }
+            $page++;
+        } while (!empty($result[0]['id']));
+
+
+        $request->session()->flash('success', 'Get data ' . $totalItem . ' items');
+        return back()->withInput();
     }
 }
