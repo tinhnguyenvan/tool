39400d79c64ed29cecf903e7f1f9e1a0a6346388
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-08-05 21:21:47 +0700

    update role

--- config/constant.php
+++ config/constant.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Role;
+
 return [
     'MENU_APP' => [
         'nav.menu_left.tool' => [
@@ -7,24 +9,28 @@ return [
             'title' => 'nav.menu_left.tool',
             'url' => '',
             'icon' => 'fa fa-code',
+            'role' => [Role::ROLE_ADMIN, Role::ROLE_MANAGER_MANAGER],
             'child' => [
                 [
                     'plugin' => 'tool',
                     'title' => 'nav.menu_left.tool_qr_code',
                     'url' => 'tools/qr_code',
                     'icon' => 'fa fa-qrcode',
+                    'role' => [Role::ROLE_ADMIN, Role::ROLE_MANAGER_MANAGER],
                 ],
                 [
                     'plugin' => 'tool',
                     'title' => 'nav.menu_left.tool_short_link',
                     'url' => 'tools/short_link',
                     'icon' => 'fa fa-link',
+                    'role' => [Role::ROLE_ADMIN, Role::ROLE_MANAGER_MANAGER],
                 ],
                 [
                     'plugin' => 'tool',
                     'title' => 'Wordpress Tool',
                     'url' => 'tools/wordpress-tool',
                     'icon' => 'fa fa-wordpress',
+                    'role' => [Role::ROLE_ADMIN, Role::ROLE_MANAGER_MANAGER],
                 ],
             ]
         ],
