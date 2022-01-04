180b2f370e17a10d30abd37ee7003402c43d1f85
tinhnguyenvan <tinhnguyenvan91@gmail.com>
2022-01-04 11:05:20 +0700

    update ui/ux admin

--- config/constant.php
+++ config/constant.php
@@ -6,12 +6,18 @@ return [
     'MENU_APP' => [
         'nav.menu_left.tool' => [
             'plugin' => 'tool',
-            'title' => 'nav.menu_left.tool_short_link',
+            'title' => 'Tool',
             'url' => 'tools/short_link',
             'icon' => 'fa fa-link',
             'role' => [Role::ROLE_ADMIN, Role::ROLE_MANAGER_MANAGER],
             'child' => [
-
+                [
+                    'plugin' => 'tool',
+                    'title' => 'nav.menu_left.tool_short_link',
+                    'url' => 'tools/short_link',
+                    'icon' => 'fa fa-link',
+                    'role' => [Role::ROLE_ADMIN, Role::ROLE_MANAGER_MANAGER],
+                ]
             ]
         ],
     ]
